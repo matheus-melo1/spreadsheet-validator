87ea57f00102f560f4ff9baac19abfa785ae7e3b
Matheus Melo <matheusmelomr@hotmail.com>
2026-03-07 17:24:35 -0300

    _

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export type { ErrorLog } from "./types/errorLog.type";
 export type { StyleTable } from "./types/styleTable.type";
 export type { ColumnInfo } from "./types/columnInfo.type";
 export type { SpreadSheetData } from "./types/spreadSheetData.type";
+// _
